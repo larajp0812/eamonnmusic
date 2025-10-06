bbb9ac8dcf769b87b6118041b271c303b4f91ed7
Lara Johnson-Perkins <lara.johnson-perkins@coodash.com>
2025-10-06 17:32:08 +0100

    Revert "https please work"
    This reverts commit 0f04379bd080da474c77d8d11dd39a65326c5e45.

--- src/Contact.js
+++ src/Contact.js
@@ -7,7 +7,7 @@ export default function Contact() {
       <div className={styles.headers}>CONTACT</div>
       <div className={styles.divider} />
       <p>For bookings, collaborations, or inquiries, please email directly:</p>
-      <a href="mailto:eamonnwmusic@gmail.com" className={styles.emailButton}>
+      <a href="mailto:your@email.com" className={styles.emailButton}>
         Email Eamonn Walsh
       </a>
     </div>
